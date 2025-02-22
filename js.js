e7e4f268322284247c8c79751783a21f06ba1740
Ricardo27885 <borgealejandro031@gmail.com>
2025-02-21 23:12:15 -0600

    Envio de correos

--- js.js
+++ js.js
@@ -5,8 +5,39 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
-
-
     document.getElementById('githubButton').addEventListener('click', function () {
         window.open('https://github.com/ricardo27885', '_blank');
+    });
+
+    document.querySelector('.btn').addEventListener('click', async function() {
+        const fromEmail = document.getElementById('from-email').value;
+        const toEmail = "borgealejandro031@gmail.com"; 
+        const subject = document.getElementById('to-Subject').value;
+        const message = document.getElementById('message').value;
+    
+        const emailData = {
+            FromEmail: fromEmail,
+            ToEmail: toEmail,
+            Subject: subject,
+            Message: message
+        };
+    
+        try {
+            const response = await fetch('https://email-xgmy.onrender.com/api/Email/send', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(emailData)
+            });
+    
+            const result = await response.json();
+            if (response.ok) {
+                alert(result.message);  // Muestra mensaje de éxito
+            } else {
+                alert(result.message);  // Muestra mensaje de error
+            }
+        } catch (error) {
+            console.error('Error en la solicitud:', error);
+        }
     });
